4c2c54cbe23dc071c785151dca2e667a66e846ef
Adam Saegebarth <adam.saegebarth@gmail.com>
2022-11-11 11:27:08 -0800

    heart rate graph

--- src/components/MatchesBurned.tsx
+++ src/components/MatchesBurned.tsx
@@ -3,21 +3,21 @@ import { Box, Flex, Text } from 'theme-ui'
 export const MatchesBurned = ({ data }) => {
   return (
     <Box>
-			<Text as="h2" variant="resultsHeading">
-				Matches Burned
-			</Text>
-      <Flex sx={{ height: ['35px', '35px', '35px'], marginTop: "20px" }}>
+      <Text as="h2" variant="resultsHeading">
+        Matches Burned
+      </Text>
+      <Flex sx={{ height: ['35px', '35px', '35px'], marginTop: '20px' }}>
         <Box sx={{ width: '34%' }}>
           <Text as="p" variant="resultsItem">
             <strong>Average Power</strong>
           </Text>
         </Box>
-        <Box sx={{ width: '33%', textAlign: "center" }}>
+        <Box sx={{ width: '33%', textAlign: 'center' }}>
           <Text as="p" variant="resultsItem">
             <strong>Total Joules</strong>
           </Text>
         </Box>
-        <Box sx={{ width: '33%', textAlign: "right"}}>
+        <Box sx={{ width: '33%', textAlign: 'right' }}>
           <Text as="p" variant="resultsItem">
             <strong>Total time</strong>
           </Text>
@@ -32,12 +32,12 @@ export const MatchesBurned = ({ data }) => {
                 {d.averagePower} watts
               </Text>
             </Box>
-            <Box sx={{ width: '33%', textAlign: "center"}}>
+            <Box sx={{ width: '33%', textAlign: 'center' }}>
               <Text as="p" variant="resultsItem">
                 {(d.totalJoules / 1000).toFixed(2)} kJ
               </Text>
             </Box>
-            <Box sx={{ width: '33%', textAlign: "right" }}>
+            <Box sx={{ width: '33%', textAlign: 'right' }}>
               <Text as="p" variant="resultsItem">
                 {d.totalTime} sec
               </Text>
