2f379a8516621b6474d7d43022ed741fcd91d776
Adam Saegebarth <adam.saegebarth@gmail.com>
2022-12-02 16:31:51 -0800

    fix up full screen carousel

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -4,34 +4,50 @@ import { getImage, GatsbyImage } from 'gatsby-plugin-image'
 import { useState } from 'react'
 
 const CustomCarousel = ({ images }: { children: JSX.Element }) => {
-  const [maximizedImage, maximizeImage] = useState('')
-  const maxi = maximizedImage ? (
-    <Box variant="styles.faded">
-      <div
-        sx={{
-          marginX: 'auto',
-          marginY: 'auto',
-        }}
-      >
-        <Box
-          sx={{ p: '0px' }}
-          onClick={() => {
-            maximizeImage('')
+  const [maximizedImage, maximizeImage] = useState(null)
+  const maxi =
+    maximizedImage !== null ? (
+      <Box variant="styles.faded">
+        <div
+          sx={{
+            marginX: 'auto',
+            marginY: 'auto',
           }}
         >
-          <Image
-            objectFit="contain"
-            image={getImage(maximizedImage)}
-            alt={''}
-            as={GatsbyImage}
-            variant="fullScreen"
-          />
-        </Box>
-      </div>
-    </Box>
-  ) : (
-    <></>
-  )
+          <Carousel
+            showArrows={true}
+            showIndicators={false}
+            showStatus={false}
+            showThumbs={false}
+            onClickItem={e => {
+              maximizeImage(null)
+            }}
+            selectedItem={maximizedImage}
+          >
+            {images.map((image, i) => (
+              <Box
+                key={`carousel-${i}`}
+                sx={{ width: '100%', position: 'relative' }}
+              >
+                <Image
+                  image={getImage(image)}
+                  alt={'blah'}
+                  as={GatsbyImage}
+                  sx={{
+                    width: ['100%', '100%', '100%'],
+                    borderRadius: [4, 4, 4],
+                    '-webkit-mask-image':
+                      '-webkit-radial-gradient(white, black)',
+                  }}
+                />
+              </Box>
+            ))}
+          </Carousel>
+        </div>
+      </Box>
+    ) : (
+      <></>
+    )
   return (
     <div>
       {maxi}
@@ -41,7 +57,8 @@ const CustomCarousel = ({ images }: { children: JSX.Element }) => {
         showStatus={false}
         showThumbs={false}
         onClickItem={e => {
-          maximizeImage(images[e])
+          console.log(e)
+          maximizeImage(e)
         }}
       >
         {images.map((image, i) => (
