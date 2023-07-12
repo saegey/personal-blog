6d04cd1acda9c1db8664d04da71b0dcc9e26e4fd
Adam Saegebarth <adam.saegebarth@gmail.com>
2023-07-11 17:35:53 -0700

    update test

--- src/lib/__tests__/myraceresult.ts
+++ src/lib/__tests__/myraceresult.ts
@@ -10,8 +10,8 @@ describe('myraceresult', () => {
         { name: 'Daniel Lincoln', time: '4:11:40.6', place: '3' },
         { name: 'Lewis Whiley', time: '4:13:07.6', place: '4' },
         { name: 'Kenneth Rizer', time: '4:18:06.5', place: '5' },
-        { name: 'Zach Winter', time: '4:18:06.7', place: '7' },
-        { name: 'Ryan Malm', time: '4:19:09.8', place: '8' }
+        { name: 'Zach Winter', time: '4:18:06.7', place: '6' },
+        { name: 'Ryan Malm', time: '4:19:09.8', place: '7' }
       ])
     })
   })
