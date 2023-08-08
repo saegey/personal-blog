2e204105c0346d10d4290e3cc1b980c4bfe27d60
Adam Saegebarth <adam.saegebarth@gmail.com>
2023-08-08 10:25:54 -0700

    Tests

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,10 @@ module.exports = {
   },
   moduleNameMapper: {
     '.+\\.(css|styl|less|sass|scss)$': 'identity-obj-proxy',
-    '.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': `<rootDir>/__mocks__/file-mock.js`,
+    '.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/__mocks__/file-mock.js',
+    '^d3-color$': '<rootDir>/node_modules/d3-color/dist/d3-color.min.js',
+    '^.+\\.svg$': 'jest-svg-transformer',
   },
   // moduleNameMapper: {
   //   '.+\\.(css|styl|less|sass|scss)$': `identity-obj-proxy`,
@@ -17,26 +20,24 @@ module.exports = {
   //   ], // Workaround for https://github.com/facebook/jest/issues/9771
   // },
   testPathIgnorePatterns: [
-    `node_modules`,
-    `\\.cache`,
-    `<rootDir>.*/public`,
-    `__test-utils__`,
+    'node_modules',
+    '\\.cache',
+    '<rootDir>.*/public',
+    '__test-utils__',
   ],
   transformIgnorePatterns: [
-    `node_modules/(?!(gatsby|gatsby-script|gatsby-link)/)`,
+    'node_modules/(?!(gatsby|gatsby-script|gatsby-link)/)',
   ],
-  moduleNameMapper: {
-    '^d3-color$': '<rootDir>/node_modules/d3-color/dist/d3-color.min.js',
-  },
   globals: {
-    __PATH_PREFIX__: ``,
+    __PATH_PREFIX__: '',
   },
   testEnvironmentOptions: {
-    url: `http://localhost`,
+    url: 'http://localhost',
   },
-  setupFiles: [`<rootDir>/loadershim.js`],
+  testEnvironment: 'jsdom',
+  setupFiles: ['<rootDir>/loadershim.js'],
   // snapshotSerializers: [
   //   '@emotion/jest/serializer' /* if needed other snapshotSerializers should go here */,
   // ],
-  setupFilesAfterEnv: [`<rootDir>/setup-test-env.js`],
+  setupFilesAfterEnv: ['<rootDir>/setup-test-env.js'],
 }
